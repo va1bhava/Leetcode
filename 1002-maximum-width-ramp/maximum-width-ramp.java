a60a5b834da429238f958344bcd670d9b2edeffe
Yoga Venkata Prasad <dvaibhava2004@gmail.com>
2026-07-24 19:58:29 +0530

    Time: 31 ms (25.53%) | Memory: 59.2 MB (95.95%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.java
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -1,19 +1,32 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
-        int n = nums.length;
-        Stack <Integer> left= new Stack <>();
-        left.push(0);
-        for(int i =1 ; i <n;i++ ){
-            if(!left.isEmpty()&&nums[i]<nums[left.peek()]){
-                left.push (i);
+        int n =  nums.length;
+       int [] s = new int[n];
+       s[n-1]=nums[n-1];
+       for(int i=n-2;i>=0;i--){
+        s[i]=Math.max(nums[i],s[i+1]);
+       }
+       int result=0 ;
+       for(int  i= 0 ;i<n;i++){
+           int max= bs(s,nums[i]);
+           result=Math.max(result,max-i);
+       }
+       return result;
+    }
+    private int bs(int[]arr , int val){
+        int l= 0;
+        int r= arr.length-1;
+        int ans= 0;
+        while(l<=r){
+            int mid= l+(r-l)/2;
+            if(arr[mid]>=val){
+                l=mid+1;
+                ans=mid;
+            }
+            else{
+                r=mid-1;
             }
         }
-        int max=0;
-        for(int i =n-1 ;i>=0;i--){
-           while(!left.isEmpty()&&nums[i]>=nums[left.peek()]){
-               max=Math.max(max,i-left.pop());
-            }
-        }
-        return max;
+        return ans; 
     }
 }
